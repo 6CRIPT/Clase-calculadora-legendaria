4acb33f3bd046e987a566cffbaae9aaa7812b9d8
6Cript <102388136+6CRIPT@users.noreply.github.com>
2022-03-26 00:07:35 +0100

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,10 +1,9 @@
 
-
 import java.util.Scanner;
 /**
  * Write a description of class calculadora here.
  * 
- * @author (your name) 
+ * @author DON 6CRIPT
  * @version (a version number or a date)
  */
 public class calculadora
@@ -13,7 +12,7 @@ public class calculadora
         Scanner t1 = new Scanner(System.in);
         System.out.println("Hola buenas tardes");
         while(0<1){
-            System.out.println("Introduzca la op a realizar: 1 potencia, 2 factorial, 0 cierra el programa");
+            System.out.println("Introduzca la op a realizar: 1 potencia, 2 factorial, 3 suma, 4 resta, 4 multiplicacion 0 cierra el programa");
             int d = t1.nextInt();
             switch(d){
                 case 1:
@@ -22,6 +21,7 @@ public class calculadora
                 int d2 = t1.nextInt();
                 System.out.println("Resultado");
                 System.out.println(potencia(d1,d2));
+                System.out.println("*****************************************************************************************************************");
                 break;
                 
                 case 2:
@@ -29,10 +29,39 @@ public class calculadora
                 int d3= t1.nextInt();
                 System.out.println("Resultado");
                 System.out.println(factorial(d3));
+                System.out.println("*****************************************************************************************************************");
+                break;
+                
+                case 3:
+                System.out.println("Introduzca los 2 numeros a sumar");
+                int d4 = t1.nextInt();
+                int d5 = t1.nextInt();
+                System.out.println("Resultado");
+                System.out.println(suma(d4, d5));
+                System.out.println("*****************************************************************************************************************");
+                break;
+                
+                case 4:
+                System.out.println("Introduzca los 2 numeros a restar");
+                int d6 = t1.nextInt();
+                int d7 = t1.nextInt();
+                System.out.println("Resultado");
+                System.out.println(resta(d6, d7));
+                System.out.println("*****************************************************************************************************************");
+                break;
+                
+                case 5: 
+                System.out.println("Introduzca los 2 numeros a multiplicar");
+                int d4 = t1.nextInt();
+                int d5 = t1.nextInt();
+                System.out.println("Resultado");
+                System.out.println(mult(d4, d5));
+                System.out.println("*****************************************************************************************************************");
                 break;
                 
                 case 0: 
                 System.out.println("Gracias por confiar en nuestro programa");
+                System.out.println("*****************************************************************************************************************");
                 System.exit(0);
                 
                 default:
@@ -42,7 +71,18 @@ public class calculadora
             }
         }
     }
-
+    
+    public static int suma (int n, int t){
+        return n + t;
+    }
+    
+    public static int resta (int n, int t){
+        return n - t;
+    }
+    
+    public static int mult(int n, int t){
+        return n*t;
+    }
     
     public static int factorial(int n){
         if(n==0){return 1;}
